395309fb2dee6121c032ebdd96e1a3fce2b1fded
RuiMochila <rui.mochila@gmail.com>
2013-06-12 15:56:37 +0100

    tudo ok

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,17 +2,20 @@
 #
 # Table name: transactions
 #
-#  id             :integer          not null, primary key
-#  token          :string(255)
-#  payer_id       :string(255)
-#  value          :float
-#  transaction_id :string(255)
-#  payment_state  :string(255)
-#  product_id     :integer
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  user_email     :string(255)
-#  user_name      :string(255)
+#  id               :integer          not null, primary key
+#  token            :string(255)
+#  payer_id         :string(255)
+#  value            :float
+#  transaction_id   :string(255)
+#  payment_state    :string(255)
+#  product_id       :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_email       :string(255)
+#  user_name        :string(255)
+#  checkout_details :string(255)
+#  color            :string(255)
+#  size             :string(255)
 #
 
 class Transaction < ActiveRecord::Base
